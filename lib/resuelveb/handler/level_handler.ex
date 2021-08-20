5c841330b979b54a7a53c862c6aea3731df70dab
Ruben Baeza <ruben.baeza@santiago.mx>
2021-08-20 17:33:49 -0500

    calculate functions

--- lib/resuelveb/handler/level_handler.ex
+++ lib/resuelveb/handler/level_handler.ex
@@ -17,5 +17,61 @@ defmodule Resuelveb.LevelHandler do
     ]
   end
 
+  def calculate(data, levels) do
+    data = data |> Poison.decode!
+    team = data["jugadores"]
+    |> Enum.group_by(fn v -> v["equipo"] end)
+    |> Enum.map(fn {_k,  v} -> v |> reckoner(levels) end)
+    json = team
+    |> List.flatten
+    |> build_json()
+    %{team: team, json: json}
+  end
+
+  def build_json(json) do
+    %{"jugadores" => json} |> Poison.encode!
+  end
+
+  def reckoner(arr, levels) do
+    goal_total_team = arr
+    |> get_goal_total_team
+    goal_total_team_ideal = arr
+    |> get_goal_total_team_ideal(levels)
+    total_team = goal_total_team / goal_total_team_ideal
+    arr
+    |> calcule_individual(total_team, levels, [])
+  end
+
+  defp calcule_individual([player | others], total_team, levels, new_arr) do
+    total_individual = player["goles"] / (player["nivel"] |> get_levels_value(levels))
+    bond = ((total_team + total_individual) / 2) * player["bono"]
+    player = player
+    |> Map.put("sueldo_completo", bond + player["sueldo"])
+    calcule_individual(others, total_team, levels, new_arr ++ [player])
+  end
+
+  defp calcule_individual([], _total_team, _levels, new_arr), do: new_arr
+
+  defp get_goal_total_team(arr) do
+    arr
+    |> Enum.map(fn g -> g["goles"] end)
+    |> Enum.sum
+  end
+
+  defp get_goal_total_team_ideal(arr, levels) do
+    arr
+    |> Enum.map(fn g -> g["nivel"] |> get_levels_value(levels) end)
+    |> Enum.sum
+  end
+
+  defp get_levels_value(name, levels) do
+    levels
+    |> Enum.find(fn m -> m.name == name end)
+    |> case do
+      nil -> 0
+      val -> val.value
+    end
+  end
+
 
 end
